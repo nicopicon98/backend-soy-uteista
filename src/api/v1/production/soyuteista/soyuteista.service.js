92b64519bff09474a6ea8de35bba3e8046d792f8
David Gómez <david.gomez@amovil.co>
2023-01-28 22:39:45 -0500

    clg

--- src/api/v1/production/soyuteista/soyuteista.service.js
+++ src/api/v1/production/soyuteista/soyuteista.service.js
@@ -25,6 +25,7 @@ const scheduleEntrada = async (req, res = response) => {
 const professionalsByFieldEntrada = async (req, res = response) => {
   const field = req.query.field;
   const resp = await executeQuery("SELECT * FROM areas INNER JOIN usuarios ON usuarios.id_area = areas.id_area WHERE areas.nombre = ?", [field])
+  console.log(resp);
   res.json(resp);
 };
 
