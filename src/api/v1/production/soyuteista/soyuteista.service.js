15e546cb9d83fb1f626291cba0f12dfe705ef7d7
David Gómez <david.gomez@amovil.co>
2023-01-28 23:40:43 -0500

    datra

--- src/api/v1/production/soyuteista/soyuteista.service.js
+++ src/api/v1/production/soyuteista/soyuteista.service.js
@@ -28,7 +28,7 @@ const professionalsByFieldEntrada = async (req, res = response) => {
   const field = req.query.field;
   const con = new mysqlConnection()
   const resp = await con.executeQuery("SELECT * FROM areas INNER JOIN usuarios ON usuarios.id_area = areas.id_area WHERE areas.nombre = ?", [field])
-  res.json(resp);
+  res.json({data: resp});
 };
 
 module.exports = {
