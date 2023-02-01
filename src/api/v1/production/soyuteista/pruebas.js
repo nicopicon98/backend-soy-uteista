35f0e201c548276d9d7359144cd98b4efded59e2
David Gómez <david.gomez@amovil.co>
2023-01-31 20:11:29 -0500

    resolviendo

--- src/api/v1/production/soyuteista/pruebas.js
+++ src/api/v1/production/soyuteista/pruebas.js
@@ -21,7 +21,7 @@ const con = new mysqlConnection()
 con.executeQuery("select usuarios.nombre, usuarios.id_usuario as usuariosIdUsuario,horario.id_horario, horario.fecha, horario.id_usuario, horario.id_franja as horarioIdFranja, franjas.id_franja as franjasIdFranja, franjas.nombre as nombreFranja from horario left join citas on citas.id_horario = horario.id_horario inner join usuarios ON usuarios.id_usuario = horario.id_usuario inner join franjas ON franjas.id_franja = horario.id_franja WHERE horario.id_horario not in (select id_horario from citas)", []).then(e => {
 
     const materias3 = groupBy(e, "fecha");
-
+    console.log(materias3)
     for (const x in materias3) {
         let key = x;
         let value = materias3[x];
