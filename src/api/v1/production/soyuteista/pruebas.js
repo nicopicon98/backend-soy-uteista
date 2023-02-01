b0e4d9bba3ca5e4a65b42cc4e35214c844b9e982
David Gómez <david.gomez@amovil.co>
2023-01-31 19:38:23 -0500

    resolviendo

--- src/api/v1/production/soyuteista/pruebas.js
+++ src/api/v1/production/soyuteista/pruebas.js
@@ -1,5 +1,8 @@
-const { scheduleByProfessional } = require("./soyuteista.service");
+const { mysqlConnection } = require("../../../../common/conexiones/conexionMysql");
 
-scheduleByProfessional().then(e => {
-    console.log(e);
+
+const con = new mysqlConnection()
+con.executeQuery("select usuarios.nombre, usuarios.id_usuario as usuariosIdUsuario,horario.id_horario, horario.fecha, horario.id_usuario, horario.id_franja as horarioIdFranja, franjas.id_franja as franjasIdFranja, franjas.nombre as nombreFranja from horario left join citas on citas.id_horario = horario.id_horario inner join usuarios ON usuarios.id_usuario = horario.id_usuario inner join franjas ON franjas.id_franja = horario.id_franja WHERE horario.id_horario not in (select id_horario from citas)", []).then(e => {
+    console.log(e)
 })
+  
