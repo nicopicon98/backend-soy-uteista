02f491cfdf23f64d45680d30b31d168dbd950000
David Gómez <david.gomez@amovil.co>
2023-01-31 20:44:41 -0500

    organizar

---
+++ src/common/utils/organizarHorarioBienestar.js
@@ -0,0 +1,35 @@
+
+const organizarHorarioBienestar = (e) => {
+    let array2 = [];
+    const materias3 = groupBy(e, "fecha");
+    for (const x in materias3) {
+        let key = x;
+        let value = materias3[x];
+        array2.push({
+            date: key,
+            franjas: value,
+        });
+    }
+    let cortes = null;
+    let array3 = [];
+
+
+    array2.forEach((e, index) => {
+        cortes = groupBy(e.franjas, "franjasIdFranja");
+
+        for (const key in cortes) {
+            array3.push({
+                id_horario: key,
+                nombre: cortes[key][0].nombreFranja,
+            });
+        }
+        array2[index].franjas = [...array3];
+        array3 = [];
+    })
+
+    return array2;
+}
+
+module.exports = {
+    organizarHorarioBienestar
+}
