34181d6d02c89a048ee34890ad8799b226a337af
David Gómez <davidgggomez0421@gmail.com>
2023-03-11 15:54:00 -0500

    correccion de clusters

--- src/app.module.js
+++ src/app.module.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cluster = require('cluster');
+const os = require('os');
 const cors = require('cors');
 const path = require('path');
 const { decryptMiddleware } = require('./api/v1/production/bienestar/middlewares/decrypt.middleware');
@@ -13,4 +15,26 @@ app.use(cors())
    .use(decryptMiddleware)
    .use(bienestarVersion(), bienestar)
    .use(express.static(path.join(__dirname, 'public')))
-   .listen(9091)
+
+if (cluster.isWorker) {
+   // Iniciar el servidor Express
+   app.listen(9091, () => {
+      console.log(`Worker ${process.pid} iniciado en el puerto ${server.address().port}`);
+   });
+}
+
+if (cluster.isMaster) {
+   // Obtener el número de CPU disponibles en el sistema
+   const numCPUs = os.cpus().length;
+
+   // Crear un worker para cada CPU
+   for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+   }
+
+   // Escuchar eventos de cambio de estado de los workers
+   cluster.on('exit', (worker, code, signal) => {
+      console.log(`Worker ${worker.process.pid} terminado`);
+      cluster.fork();
+   });
+}
