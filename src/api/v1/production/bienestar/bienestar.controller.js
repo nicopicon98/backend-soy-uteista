a4307cd931fc0dc9a8fba81913f50896388d9d06
David Gómez <davidgggomez0421@gmail.com>
2023-03-11 13:05:50 -0500

    declaracion de endpoints terminada

--- src/api/v1/production/bienestar/bienestar.controller.js
+++ src/api/v1/production/bienestar/bienestar.controller.js
@@ -1,14 +1,37 @@
 const express = require("express");
 const bienestar = express.Router();
-const { login, register } = require("./bienestar.service");
+const {
+  login,
+  register,
+  assignLocation,
+  createScheduleByProfessional,
+  getScheduleByProfessional,
+  createNewService,
+  deleteNewService,
+  rejectDate,
+  closeDateByProfessional,
+  closeDateByStudent,
+  nextPastDatesByProfessional,
+  lastDateByProfessional,
+} = require("./bienestar.service");
 
 bienestar.use((req, res, next) => {
-    next();
+  next();
 });
 
-bienestar.get("/login", login);
-bienestar.get("/register", register);
+bienestar.post("/login", login);
+bienestar.post("/register", register);
+bienestar.post("/assign-location", assignLocation);
+bienestar.post("/create-schedule-by-professional", createScheduleByProfessional);
+bienestar.post("/get-schedule-by-professional", getScheduleByProfessional);
+bienestar.post("/create-new-service", createNewService);
+bienestar.post("/delete-new-service", deleteNewService);
+bienestar.post("/reject-date", rejectDate);
+bienestar.post("/close-date-by-professional", closeDateByProfessional);
+bienestar.post("/close-date-by-student", closeDateByStudent);
+bienestar.post("/next-past-dates-by-professional", nextPastDatesByProfessional);
+bienestar.post("/last-date-by-professional", lastDateByProfessional);
 
 module.exports = {
-    bienestar,
+  bienestar,
 };
