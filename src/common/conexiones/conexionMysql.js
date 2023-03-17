5c6ccfa12b1150865d1699ae1bb462b41955f883
David Gómez <davidgggomez0421@gmail.com>
2023-03-16 20:48:02 -0500

    correccion de llamado

--- src/common/conexiones/conexionMysql.js
+++ src/common/conexiones/conexionMysql.js
@@ -1,15 +1,14 @@
-const mysql = require('mysql');
+const mysqlLib = require("mysql");
 class mysqlConnection {
-  constructor() {
-  }
+  constructor() {}
 
   connectedDB() {
-    this.connection = mysql.createConnection({
+    this.connection = mysqlLib.createConnection({
       host: "localhost",
       user: "root",
       password: "06f60ff6f",
       database: "bienestar",
-      port: 3306
+      port: 3306,
     });
   }
 
@@ -20,25 +19,23 @@ class mysqlConnection {
   /** Ejecutar query */
   executeQuery(query, params = []) {
     return new Promise((resolve, reject) => {
-      this.connectedDB()
+      this.connectedDB();
       try {
-        this.connection.query(query, params,
-          function (error, results, fields) {
-            resolve(results)
-            reject(error)
-          })
-        
+        this.connection.query(query, params, function (error, results, fields) {
+          resolve(results);
+          reject(error);
+        });
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
 
-      this.disconnectedDB()
+      this.disconnectedDB();
     });
   }
 }
 
-const mysql = new mysqlConnection()
+const mysql = new mysqlConnection();
 
 module.exports = {
-  mysql
-}
+  mysql,
+};
