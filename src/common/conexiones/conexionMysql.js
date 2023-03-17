ed18fecfd035c9326dd458b56d9b939b9b73af86
David Gómez <davidgggomez0421@gmail.com>
2023-03-16 20:49:41 -0500

    correccion de llamado

--- src/common/conexiones/conexionMysql.js
+++ src/common/conexiones/conexionMysql.js
@@ -1,41 +1,57 @@
-const mysqlLib = require("mysql");
+const mysql = require("mysql");
+
 class mysqlConnection {
   constructor() {}
 
-  connectedDB() {
-    this.connection = mysqlLib.createConnection({
+  connect() {
+    this.connection = mysql.createConnection({
       host: "localhost",
       user: "root",
       password: "06f60ff6f",
       database: "bienestar",
       port: 3306,
     });
-  }
 
-  disconnectedDB() {
-    this.connection.end();
-  }
-
-  /** Ejecutar query */
-  executeQuery(query, params = []) {
-    return new Promise((resolve, reject) => {
-      this.connectedDB();
-      try {
-        this.connection.query(query, params, function (error, results, fields) {
-          resolve(results);
-          reject(error);
-        });
-      } catch (error) {
-        console.log(error);
+    this.connection.connect(function (err) {
+      if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
       }
 
-      this.disconnectedDB();
+      console.log("connected as id " + connection.threadId);
+    });
+  }
+
+  disconnect() {
+    this.connection.end(function (err) {
+      if (err) {
+        console.error("error disconnecting: " + err.stack);
+        return;
+      }
+
+      console.log("disconnected from database");
+    });
+  }
+
+  executeQuery(query, params = []) {
+    return new Promise((resolve, reject) => {
+      this.connect();
+
+      this.connection.query(query, params, function (error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      });
+
+      this.disconnect();
     });
   }
 }
 
-const mysql = new mysqlConnection();
+const mysqlInstance = new mysqlConnection();
 
 module.exports = {
-  mysql,
+  mysql: mysqlInstance,
 };
