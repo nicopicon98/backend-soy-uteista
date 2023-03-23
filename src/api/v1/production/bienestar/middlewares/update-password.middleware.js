44df753ca8fa692514e42d66433d87027c071e6c
David Gomez <david.gomez@amovil.co>
2023-03-23 12:48:38 -0500

    update password middleware

---
+++ src/api/v1/production/bienestar/middlewares/update-password.middleware.js
@@ -0,0 +1,30 @@
+const { body, validationResult } = require("express-validator");
+const { send } = require("../config/crypto.config");
+
+const updatePasswordValidationRules = () => {
+  return [
+    body("id_usuario")
+      .notEmpty()
+      .withMessage("El campo id_usuario es obligatorio")
+      .isInt()
+      .withMessage("El campo id_usuario debe ser un número entero"),
+    body("password")
+      .notEmpty()
+      .withMessage("El objeto schedule es obligatorio")
+      .isLength({ min: 8 })
+      .withMessage("schedule debe ser un objeto"),
+  ];
+};
+
+const validateUpdatePassword = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  return send({ errors: errors.array() }, res);
+};
+
+module.exports = {
+  updatePasswordValidationRules,
+  validateUpdatePassword,
+};
