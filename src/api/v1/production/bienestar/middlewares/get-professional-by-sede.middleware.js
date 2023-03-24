a5b0d8f4f65c54a8af43f24089053b0623508b0e
David Gómez <davidgggomez0421@gmail.com>
2023-03-24 02:59:47 -0500

    cambio a string

--- src/api/v1/production/bienestar/middlewares/get-professional-by-sede.middleware.js
+++ src/api/v1/production/bienestar/middlewares/get-professional-by-sede.middleware.js
@@ -12,6 +12,7 @@ const getProfessionalBySedeValidationRules = () => {
 };
 
 const validateGetProfessionalBySede = (req, res, next) => {
+  console.log(req.body)
   const errors = validationResult(req);
   if (errors.isEmpty()) {
     return next();
