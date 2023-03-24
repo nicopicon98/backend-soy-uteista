02bffe69d3a1716f69bc7b2827d367a6cccfe57c
David Gómez <davidgggomez0421@gmail.com>
2023-03-24 02:57:26 -0500

    cambio a string

--- src/api/v1/production/bienestar/middlewares/get-professional-by-sede.middleware.js
+++ src/api/v1/production/bienestar/middlewares/get-professional-by-sede.middleware.js
@@ -6,7 +6,7 @@ const getProfessionalBySedeValidationRules = () => {
     body("id_campus_area")
       .notEmpty()
       .withMessage("El campo id_usuario es obligatorio")
-      .isInt()
+      .isString()
       .withMessage("El campo nombre_campus debe ser un string"),
   ];
 };
