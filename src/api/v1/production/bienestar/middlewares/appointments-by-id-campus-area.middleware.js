3da50246cd522000a85597ecd6f4ad74c11d7cbe
David Gómez <davidgggomez0421@gmail.com>
2023-03-23 23:24:08 -0500

    idk

---
+++ src/api/v1/production/bienestar/middlewares/appointments-by-id-campus-area.middleware.js
@@ -0,0 +1,30 @@
+const { body, validationResult } = require("express-validator");
+const { send } = require("../config/crypto.config");
+
+const appointmentsByIdCampusAreaValidationRules = () => {
+  return [
+    body("tomado_por")
+      .notEmpty()
+      .withMessage("El campo tomado_por es obligatorio")
+      .isEmail()
+      .withMessage("El campo tomado_por debe ser un correo electrónico válido"),
+    body("id_campus")
+      .notEmpty()
+      .withMessage("El campo id_campus es obligatorio")
+      .isInt()
+      .withMessage("El campo id_campus debe ser un número entero"),
+  ];
+};
+
+const validateAppointmentsByIdCampusArea = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  return send({ error: [errors.array()[0].msg], status: 406 }, res);
+};
+
+module.exports = {
+  appointmentsByIdCampusAreaValidationRules,
+  validateAppointmentsByIdCampusArea,
+};
