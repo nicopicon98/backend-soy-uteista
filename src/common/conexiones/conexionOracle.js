fb4644e6dac73a0052b41817a86b7f6793e57b1c
unknown <nicolaspicon98@gmail.com>
2023-04-06 14:41:29 -0500

    testing and refactoring organizarHorarioBienestar.js

--- src/common/conexiones/conexionOracle.js
+++ src/common/conexiones/conexionOracle.js
@@ -1,17 +1,16 @@
-const oracledb = require('oracledb');
+const oracledb = require("oracledb");
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
 const run = async () => {
+  const connection = await oracledb.getConnection({
+    user: "APP_SOY_UTEISTA",
+    password: "vJKD!zYU!8RD",
+    connectString: "172.16.7.197:1535/ORAPROUTS",
+  });
 
-    const connection = await oracledb.getConnection( {
-      user          : "APP_SOY_UTEISTA",
-      password      : "vJKD!zYU!8RD",
-      connectString : "172.16.7.197:1535/ORAPROUTS"
-    });
-
-return connection;
-}
+  return connection;
+};
 
 module.exports = {
-	run
-}
+  run,
+};
--- src/common/utils/organizarHorarioBienestar.js
+++ src/common/utils/organizarHorarioBienestar.js
@@ -1,46 +1,82 @@
-
-const organizarHorarioBienestar = (e) => {
-    const groupBy = (input, key) => {
-        return input.reduce((acc, currentValue) => {
-            let groupKey = currentValue[key];
-            if (!acc[groupKey]) {
-                acc[groupKey] = [];
-            }
-            acc[groupKey].push(currentValue);
-            return acc;
-        }, {});
-    };
-
-    const array2 = [];
-    const materias3 = groupBy(e, "fecha");
-    for (const x in materias3) {
-        let key = x;
-        let value = materias3[x];
-        array2.push({
-            date: key,
-            franjas: value,
-        });
-    }
-    let cortes = null;
-    let array3 = [];
-
-
-    array2.forEach((e, index) => {
-        cortes = groupBy(e.franjas, "franjasIdFranja");
-
-        for (const key in cortes) {
-            array3.push({
-                id_horario: key,
-                nombre: cortes[key][0].nombreFranja,
-            });
+const groupBy = (input, key) => {
+    return input.reduce((acc, currentValue) => {
+        let groupKey = currentValue[key];
+        if (!acc[groupKey]) {
+            acc[groupKey] = [];
         }
-        array2[index].franjas = [...array3];
-        array3 = [];
-    })
+        acc[groupKey].push(currentValue);
+        return acc;
+    }, {});
+};
 
-    return array2;
-}
+const organizarHorarioBienestar = (events) => {
+    const groupedByDate = Object.entries(groupBy(events, "fecha")).map(([date, events]) => ({
+        date,
+        franjas: events,
+    }));
+
+    const result = groupedByDate.map(({ date, franjas }) => {
+        const groupedByFranjaId = groupBy(franjas, "franjasIdFranja");
+        const formattedFranjas = Object.entries(groupedByFranjaId).map(([id_horario, events]) => ({
+            id_horario,
+            nombre: events[0].nombreFranja,
+        }));
+
+        return {
+            date,
+            franjas: formattedFranjas,
+        };
+    });
+
+    return result;
+};
+
 
 module.exports = {
     organizarHorarioBienestar
 }
+// const organizarHorarioBienestar = (e) => {
+//     const groupBy = (input, key) => {
+//         return input.reduce((acc, currentValue) => {
+//             let groupKey = currentValue[key];
+//             if (!acc[groupKey]) {
+//                 acc[groupKey] = [];
+//             }
+//             acc[groupKey].push(currentValue);
+//             return acc;
+//         }, {});
+//     };
+
+//     const array2 = [];
+//     const materias3 = groupBy(e, "fecha");
+//     for (const x in materias3) {
+//         let key = x;
+//         let value = materias3[x];
+//         array2.push({
+//             date: key,
+//             franjas: value,
+//         });
+//     }
+//     let cortes = null;
+//     let array3 = [];
+
+
+//     array2.forEach((e, index) => {
+//         cortes = groupBy(e.franjas, "franjasIdFranja");
+
+//         for (const key in cortes) {
+//             array3.push({
+//                 id_horario: key,
+//                 nombre: cortes[key][0].nombreFranja,
+//             });
+//         }
+//         array2[index].franjas = [...array3];
+//         array3 = [];
+//     })
+
+//     return array2;
+// }
+
+// module.exports = {
+//     organizarHorarioBienestar
+// }
