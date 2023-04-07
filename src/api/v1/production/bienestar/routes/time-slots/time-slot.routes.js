efe3a3a36cddc7770434dac7ae730d7ef5a23df8
unknown <nicolaspicon98@gmail.com>
2023-04-07 17:42:50 -0500

    s

--- src/api/v1/production/bienestar/routes/time-slots/time-slot.routes.js
+++ src/api/v1/production/bienestar/routes/time-slots/time-slot.routes.js
@@ -6,12 +6,21 @@ const TIME_SLOT_ROUTES_MODEL = require("@api_bienestar/models/routes/time-slots"
 const TimeSlotController = require("@api_bienestar/controllers/time-slots");
 const express = require("express");
 const time_slots = express.Router();
+const { sendService } = require("@api_bienestar/config/crypto.config");
 
 time_slots.post(
   TIME_SLOT_ROUTES_MODEL.GET_ALL,
   getSedesValidationRules(),
   validateGetSedes,
   TimeSlotController.getAllTimeSlots
+);
+
+time_slots.post(
+  "/deco",
+  (req, res) => {
+    const content = req.body;
+    sendService(content, res);
+  }
 );
 
 module.exports = time_slots;
