9f7484eb7fc871e4915c60da17241bbd14ca5406
unknown <nicolaspicon98@gmail.com>
2023-04-07 12:57:39 -0500

    structure

--- src/api/v1/production/bienestar/routes/time_slot.routes.js
+++ src/api/v1/production/bienestar/routes/time_slot.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const timeSlotController = require('../controllers/time_slots/time-slot.controller');
 
-router.get('/get-all-time-slots', timeSlotController.getAllTimeSlots);
+router.post('/get-all-time-slots', timeSlotController.getAllTimeSlots);
 // router.post('/get-time-slots-by-professional', timeSlotController.getTimeSlotsByProfessional);
 
 module.exports = router;
