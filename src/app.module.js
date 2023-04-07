34e756ba5a68c21710711b2011a63e07de762582
unknown <nicolaspicon98@gmail.com>
2023-04-07 15:50:08 -0500

    structure

--- src/app.module.js
+++ src/app.module.js
@@ -15,8 +15,8 @@ const app = express();
 const { decryptMiddleware } = require('./api/v1/production/bienestar/middlewares/decrypt.middleware');
 
 // Import routes
-const { bienestarVersion, bienestar } = require('./api/v1/production/bienestar');
-const { soyUteistaVersion, soyuteista } = require('./api/v1/production/soyuteista');
+// const { bienestarVersion, bienestar } = require('./api/v1/production/bienestar');
+const { soyUteistaVersion, soyuteista } = require('@api_v1_production_soyuteista');
 
 // Configure app middleware
 app.use(cors())
@@ -24,7 +24,7 @@ app.use(cors())
    .use(soyUteistaVersion(), soyuteista)
    .use(decryptMiddleware)
    .use(morgan('dev'))
-   .use(bienestarVersion(), bienestar)
+  //  .use(bienestarVersion(), bienestar)
    .use(express.static(path.join(__dirname, 'public')));
 
 // Start the server listening on port 9091
