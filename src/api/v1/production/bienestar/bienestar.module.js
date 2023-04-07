b065d37365062dc6cb1b4b40579f1892e22f29fc
unknown <nicolaspicon98@gmail.com>
2023-04-07 16:00:52 -0500

    s

--- src/api/v1/production/bienestar/bienestar.module.js
+++ src/api/v1/production/bienestar/bienestar.module.js
@@ -2,6 +2,14 @@ const bienestarVersion = () => {
   return '/api/v1/production/bienestar';
 };
 
+const express = require('express');
+const bienestar = express();
+
+const timeSlotRoutes = require('./routes/time_slot.routes');
+
+bienestar.use(timeSlotRoutes);
+
 module.exports = {
+  bienestar,
   bienestarVersion
 }
--- src/api/v1/production/bienestar/index.js
+++ src/api/v1/production/bienestar/index.js
@@ -1,11 +1,4 @@
-const { bienestarVersion } = require('./bienestar.module');
-
-const express = require('express');
-const bienestar = express();
-
-const timeSlotRoutes = require('./routes/time_slot.routes');
-
-bienestar.use(timeSlotRoutes);
+const { bienestarVersion, bienestar } = require('./bienestar.module');
 
 module.exports = {
   bienestar,
