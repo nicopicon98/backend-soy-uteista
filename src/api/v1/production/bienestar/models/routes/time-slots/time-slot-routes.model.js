48d7facdc243d68dbd3a74cad5fd41b7906b1d23
unknown <nicolaspicon98@gmail.com>
2023-04-07 17:47:59 -0500

    adding get-all-time-slots-by-professional endpoint

--- src/api/v1/production/bienestar/models/routes/time-slots/time-slot-routes.model.js
+++ src/api/v1/production/bienestar/models/routes/time-slots/time-slot-routes.model.js
@@ -1,5 +1,6 @@
 const TIME_SLOT_ROUTES_MODEL = {
   GET_ALL: "/get-all-time-slots",
+  GET_ALL_BY_PROFESSIONAL: "/get-all-time-slots-by-professional"
 };
 
 module.exports = TIME_SLOT_ROUTES_MODEL;
--- src/api/v1/production/bienestar/routes/time-slots/time-slot.routes.js
+++ src/api/v1/production/bienestar/routes/time-slots/time-slot.routes.js
@@ -4,15 +4,22 @@ const {
 } = require("@api_bienestar/middlewares/get-sedes.middleware");
 const TIME_SLOT_ROUTES_MODEL = require("@api_bienestar/models/routes/time-slots");
 const TimeSlotController = require("@api_bienestar/controllers/time-slots");
+const { sendService } = require("@api_bienestar/config/crypto.config");
 const express = require("express");
 const time_slots = express.Router();
-const { sendService } = require("@api_bienestar/config/crypto.config");
 
 time_slots.post(
   TIME_SLOT_ROUTES_MODEL.GET_ALL,
   getSedesValidationRules(),
   validateGetSedes,
   TimeSlotController.getAllTimeSlots
+);
+
+time_slots.post(
+  TIME_SLOT_ROUTES_MODEL.GET_ALL_BY_PROFESSIONAL,
+  getSedesValidationRules(),
+  validateGetSedes,
+  TimeSlotController.getTimeSlotsByProfessional
 );
 
 time_slots.post(
