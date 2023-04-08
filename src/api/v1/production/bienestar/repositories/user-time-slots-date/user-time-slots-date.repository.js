3805f97187c6d96698f6c08a2945dbc2acafd862
unknown <nicolaspicon98@gmail.com>
2023-04-07 23:09:20 -0500

    s

--- src/api/v1/production/bienestar/repositories/user-time-slots-date/user-time-slots-date.repository.js
+++ src/api/v1/production/bienestar/repositories/user-time-slots-date/user-time-slots-date.repository.js
@@ -34,6 +34,7 @@ class UserTimeSlotsDateRepository {
     for (const date = new Date(startDate); date <= new Date(endDate); date.setDate(date.getDate() + 1)) {
       for (const time_slot_id of time_slots) {
         const rows = await mysql.executeQuery(sql, [id_user, date, time_slot_id]);
+        console.log(rows)
         affectedRows += rows.affectedRows;
       }
     }
