fec1ef6d79ddff3cfde664334b6a148a6dcd920e
unknown <nicolaspicon98@gmail.com>
2023-04-07 23:23:15 -0500

    s

--- src/api/v1/production/bienestar/repositories/user-time-slots-date/user-time-slots-date.repository.js
+++ src/api/v1/production/bienestar/repositories/user-time-slots-date/user-time-slots-date.repository.js
@@ -22,36 +22,36 @@ class UserTimeSlotsDateRepository {
     return mysql.executeQuery(query, [professional_id]);
   }
 
-  static async insert(id_user, user_time_slots_date) {
-    const { startDate, endDate, time_slots } = user_time_slots_date;
+  // static async insert(id_user, user_time_slots_date) {
+  //   const { startDate, endDate, time_slots } = user_time_slots_date;
     
-    const sql = `
-      INSERT INTO user_time_slots_date (id_user, date, id_time_slot)
-      VALUES (?, ?, ?)
-    `;
-    let affectedRows = 0;
-    for (let date = new Date(startDate); date <= new Date(endDate); date.setDate(date.getDate() + 1)) {
-      for (const id_time_slot of time_slots) {
-        const rows = await mysql.executeQuery(sql, [id_user, date, id_time_slot]);
-        console.log(rows, "rows")
-        affectedRows += rows.affectedRows;
-      }
-    }
-    return affectedRows > 0 ? { message: "User time slots date inserted successfully" } : { message: "Failed to insert user time slots date" };
-  }
-
-  // static async testInsert() {
-  //   try {
-  //     const sql = `
-  //       INSERT INTO user_time_slots_date (id_user, date, id_time_slot)
-  //       VALUES (?, ?, ?)
-  //     `;
-  //     const [rows] = await mysql.executeQuery(sql, [1, '2023-04-25', 1]);
-  //     console.log(rows);
-  //   } catch (error) {
-  //     console.error("Error: ", error);
+  //   const sql = `
+  //     INSERT INTO user_time_slots_date (id_user, date, id_time_slot)
+  //     VALUES (?, ?, ?)
+  //   `;
+  //   let affectedRows = 0;
+  //   for (let date = new Date(startDate); date <= new Date(endDate); date.setDate(date.getDate() + 1)) {
+  //     for (const id_time_slot of time_slots) {
+  //       const rows = await mysql.executeQuery(sql, [id_user, date, id_time_slot]);
+  //       console.log(rows, "rows")
+  //       affectedRows += rows.affectedRows;
+  //     }
   //   }
+  //   return affectedRows > 0 ? { message: "User time slots date inserted successfully" } : { message: "Failed to insert user time slots date" };
   // }
+
+  static async testInsert() {
+    try {
+      const sql = `
+        INSERT INTO user_time_slots_date (id_user, date, id_time_slot)
+        VALUES (?, ?, ?)
+      `;
+      const [rows] = await mysql.executeQuery(sql, [1, '2023-04-25', 1]);
+      console.log(rows);
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  }
   
 
   
