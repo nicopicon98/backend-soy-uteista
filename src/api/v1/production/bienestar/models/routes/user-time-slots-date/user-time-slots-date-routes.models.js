20c3fc29e9423c12f3d181d132ccbd9193a754ab
unknown <nicolaspicon98@gmail.com>
2023-04-07 20:40:04 -0500

    s

--- src/api/v1/production/bienestar/models/routes/user-time-slots-date/user-time-slots-date-routes.models.js
+++ src/api/v1/production/bienestar/models/routes/user-time-slots-date/user-time-slots-date-routes.models.js
@@ -1,5 +1,5 @@
 const USER_TIME_SLOTS_DATE_ROUTES = {
-  GET_ALL_BY_PROFESSIONAL: "get-all-user-time-slots-date-by-professional",
+  GET_ALL_BY_PROFESSIONAL: "/get-all-user-time-slots-date-by-professional",
 };
 
 module.exports = USER_TIME_SLOTS_DATE_ROUTES;
