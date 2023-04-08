d5f142bf28e733cc7d14f5cd58b508faf8c856f0
unknown <nicolaspicon98@gmail.com>
2023-04-08 18:29:04 -0500

    s

--- src/common/security/bcrypt_encryption.js
+++ src/common/security/bcrypt_encryption.js
@@ -2,8 +2,10 @@ const bcrypt = require("bcrypt");
 
 const hashPassword = async (password) => {
   const saltRounds = 10;
+  console.log('Password:', password, 'Salt Rounds:', saltRounds); // Add this line
   try {
     const hashedPassword = await bcrypt.hash(password, saltRounds);
+    console.log(hashedPassword, "hashedPassword")
     return hashedPassword;
   } catch (error) {
     console.error("Error hashing password:", error);
