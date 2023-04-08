77adb149fb9e28dc0bbce32cab658b91d09707cf
unknown <nicolaspicon98@gmail.com>
2023-04-08 17:08:54 -0500

    s

--- src/api/v1/production/bienestar/models/routes/user/user-routes.model.js
+++ src/api/v1/production/bienestar/models/routes/user/user-routes.model.js
@@ -2,7 +2,7 @@ const USER_ROUTES_MODEL = {
   GET_ALL_PROFESSIONALS: "/get-all-professionals",
   GET_ALL_PROFESSIONALS_BY_CAMPUS_FIELD:
     "/get-all-professionals-by-campus-field",
-  INSERT_PROFESSIONAL: "insert-professional",
+  INSERT_PROFESSIONAL: "/insert-professional",
 };
 
 module.exports = USER_ROUTES_MODEL;
