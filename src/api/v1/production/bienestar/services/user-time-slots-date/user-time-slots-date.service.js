b738384d93ba047f8a9b4f14f073c8557a44fa91
unknown <nicolaspicon98@gmail.com>
2023-04-07 21:14:44 -0500

    s

--- src/api/v1/production/bienestar/services/user-time-slots-date/user-time-slots-date.service.js
+++ src/api/v1/production/bienestar/services/user-time-slots-date/user-time-slots-date.service.js
@@ -3,29 +3,28 @@ const { mysql } = require("@src/common/conexiones/conexionMysql");
 class UserTimeSlotsDateService {
   static async getAllByProfessional(professional_id) {
     const query = `
-      SELECT
-        user_time_slots_date.date,
-        GROUP_CONCAT(time_slots.id_time_slot ORDER BY time_slots.id_time_slot) AS time_slot_ids,
-        GROUP_CONCAT(time_slots.name_time_slot ORDER BY time_slots.id_time_slot) AS time_slot_names,
-        GROUP_CONCAT(user_time_slots_date.id_user_time_slot_date ORDER BY time_slots.id_time_slot) AS user_time_slot_ids
-      FROM
-        user_time_slots_date
-        JOIN users ON user_time_slots_date.id_user = users.id_user
-        JOIN time_slots ON user_time_slots_date.id_time_slot = time_slots.id_time_slot
-        JOIN campuses_fields ON users.id_campuses_field = campuses_fields.id_campuses_field
-      WHERE
-        users.id_role = 2 AND campuses_fields.id_campus = ?
-      GROUP BY
-        user_time_slots_date.date
-      ORDER BY
-        user_time_slots_date.date ASC;
+    SELECT
+    user_time_slots_date.date,
+    GROUP_CONCAT(time_slots.id_time_slot ORDER BY time_slots.id_time_slot) AS time_slot_ids,
+    GROUP_CONCAT(time_slots.name_time_slot ORDER BY time_slots.id_time_slot) AS time_slot_names,
+    GROUP_CONCAT(user_time_slots_date.id_user_time_slot_date ORDER BY time_slots.id_time_slot) AS user_time_slot_ids
+  FROM
+    user_time_slots_date
+    JOIN users ON user_time_slots_date.id_user = users.id_user
+    JOIN time_slots ON user_time_slots_date.id_time_slot = time_slots.id_time_slot
+  WHERE
+    users.id_user = 1
+  GROUP BY
+    user_time_slots_date.date
+  ORDER BY
+    user_time_slots_date.date ASC;
     `;
     const [rows] = await mysql.executeQuery(query, [professional_id]);
 
     return rows.map((row) => {
-      const timeSlotIds = row.time_slot_ids.split(',');
-      const timeSlotNames = row.time_slot_names.split(',');
-      const userTimeSlotIds = row.user_time_slot_ids.split(',');
+      const timeSlotIds = row.time_slot_ids.split(",");
+      const timeSlotNames = row.time_slot_names.split(",");
+      const userTimeSlotIds = row.user_time_slot_ids.split(",");
       const timeSlots = timeSlotIds.map((id, index) => ({
         id_user_time_slot_date: parseInt(userTimeSlotIds[index]),
         id_time_slot: parseInt(id),
