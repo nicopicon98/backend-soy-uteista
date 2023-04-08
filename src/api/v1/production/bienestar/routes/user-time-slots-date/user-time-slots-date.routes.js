f60f6b1ec07cf43ce149a1ba40105df3acee2965
unknown <nicolaspicon98@gmail.com>
2023-04-07 20:27:36 -0500

    s

--- src/api/v1/production/bienestar/routes/user-time-slots-date/user-time-slots-date.routes.js
+++ src/api/v1/production/bienestar/routes/user-time-slots-date/user-time-slots-date.routes.js
@@ -1,4 +1,8 @@
-const USER_TIME_SLOTS_DATE_ROUTES = require("@api_bienestar/models/routes/user-time-slots-date");
+const {
+  validateGetSedes,
+  getSedesValidationRules,
+} = require("@api_bienestar/middlewares/get-sedes.middleware");
+const USER_TIME_SLOTS_DATE_ROUTES = require("../../models/routes/user-time-slots-date");
 const UserTimeSlotsDateController = require("../../controllers/user-time-slots-date");
 const express = require("express");
 const userTimeSlotsDateRouter = express.Router();
@@ -6,6 +10,8 @@ const userTimeSlotsDateRouter = express.Router();
 
 userTimeSlotsDateRouter.post(
   USER_TIME_SLOTS_DATE_ROUTES.GET_ALL_BY_PROFESSIONAL,
+  getSedesValidationRules(),
+  validateGetSedes,
   UserTimeSlotsDateController.getAllByProfessional
 );
 
