c9010792a37e856329ae91c09ee23ec8d9ce19ba
unknown <nicolaspicon98@gmail.com>
2023-04-08 18:27:19 -0500

    s

--- src/common/security/bcrypt_encryption.js
+++ src/common/security/bcrypt_encryption.js
@@ -1,14 +1,21 @@
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 
-const hashPassword = (plaintextPassword) => {
-    return bcrypt.hash(plaintextPassword, 10);
-}
+const hashPassword = async (password) => {
+  const saltRounds = 10;
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    console.error("Error hashing password:", error);
+    throw error;
+  }
+};
 
 const comparePassword = (plaintextPassword, hash) => {
-    return bcrypt.compare(plaintextPassword, hash);
-}
+  return bcrypt.compare(plaintextPassword, hash);
+};
 
 module.exports = {
-    hashPassword,
-    comparePassword
-}
+  hashPassword,
+  comparePassword,
+};
