a6c79cb89323bf61dba26b0ec72e876bb8c2c022
unknown <nicolaspicon98@gmail.com>
2023-04-09 12:58:09 -0500

    s

--- src/api/v1/production/bienestar/repositories/field/field.repository.js
+++ src/api/v1/production/bienestar/repositories/field/field.repository.js
@@ -1,44 +1,93 @@
 const { mysql } = require("@src/common/conexiones/conexionMysql");
 
 class FieldRepository {
+  /**
+   * Get all fields
+   *
+   * @returns {Array} - The list of all fields
+   */
   static async getAll() {
     const query = "SELECT * FROM fields";
     return mysql.executeQuery(query);
   }
 
+  /**
+   * Get all fields that are not associated with a given campus
+   *
+   * @param {number} id_campus - The ID of the campus
+   * @returns {Array} - The list of all fields that are not associated with the given campus
+   */
   static async getAllNotInCampus(id_campus) {
     const query = `
-    SELECT f.*
+      SELECT f.*
       FROM fields f
       WHERE f.id_field NOT IN (
         SELECT cf.id_field
         FROM campuses_fields cf
         WHERE cf.id_campus = ?
       )
-  `;
+    `;
     return mysql.executeQuery(query, [id_campus]);
   }
 
+  /**
+   * Insert a new field
+   *
+   * @param {string} name_field - The name of the new field
+   * @returns {object} - The newly inserted field
+   * @throws An error if a field with the same name already exists
+   */
   static async insert(name_field) {
     const query = "INSERT INTO fields (name_field) VALUES (?)";
     return mysql.executeQuery(query, [name_field]);
   }
 
+  /**
+   * Delete a field by ID
+   *
+   * @param {number} id_field - The ID of the field to delete
+   * @returns {object} - The deleted field
+   * @throws An error if the field doesn't exist or is associated with a campus
+   */
   static async delete(id_field) {
     const query = "DELETE FROM fields WHERE id_field = ?";
     return mysql.executeQuery(query, [id_field]);
   }
 
+  /**
+   * Get a field by name
+   *
+   * @param {string} name_field - The name of the field to retrieve
+   * @returns {object} - The retrieved field
+   */
   static async getByName(name_field) {
     const query = "SELECT * FROM fields WHERE LOWER(name_field) = LOWER(?)";
     const result = await mysql.executeQuery(query, [name_field]);
     return result[0];
   }
 
+  /**
+   * Check if a field is associated with a campus
+   *
+   * @param {number} id_field - The ID of the field to check
+   * @returns {boolean} - True if the field is associated with a campus, false otherwise
+   */
   static async isFieldInCampus(id_field) {
     const query = "SELECT * FROM campuses_fields WHERE id_field = ?";
     const result = await mysql.executeQuery(query, [id_field]);
     return result.length > 0;
+  }
+
+  /**
+   * Get a field by ID
+   *
+   * @param {number} id_field - The ID of the field to retrieve
+   * @returns {object} - The retrieved field
+   */
+  static async getById(id_field) {
+    const query = "SELECT * FROM fields WHERE id_field = ?";
+    const result = await mysql.executeQuery(query, [id_field]);
+    return result[0];
   }
 }
 
--- src/api/v1/production/bienestar/services/field/field.service.js
+++ src/api/v1/production/bienestar/services/field/field.service.js
@@ -1,14 +1,33 @@
 const FieldRepository = require("../../repositories/field");
 const { HTTP_HANDLING_MSGS } = require("../../utilities");
+
 class FieldService {
+  /**
+   * Get all fields
+   *
+   * @returns {Array} - The list of fields
+   */
   static async getAll() {
     return FieldRepository.getAll();
   }
 
+  /**
+   * Get all fields not associated with a specific campus
+   *
+   * @param {string} id_campus - The ID of the campus
+   * @returns {Array} - The list of fields
+   */
   static async getAllNotInCampus(id_campus) {
     return FieldRepository.getAllNotInCampus(id_campus);
   }
 
+  /**
+   * Insert a field
+   *
+   * @param {string} name_field - The name of the field
+   * @returns {Object} - The inserted field
+   * @throws {Error} - If a field with the same name already exists
+   */
   static async insert(name_field) {
     const existingField = await FieldRepository.getByName(name_field);
     if (existingField) {
@@ -21,8 +40,16 @@ class FieldService {
     return FieldRepository.insert(name_field);
   }
 
+  /**
+   * Delete a field
+   *
+   * @param {string} id_field - The ID of the field
+   * @returns {Object} - The deleted field
+   * @throws {Error} - If the field does not exist or if it is associated with a campus
+   */
   static async delete(id_field) {
-    const existingField = await FieldRepository.isFieldInCampus(id_field);
+    // Validate that the field exists
+    const existingField = await FieldRepository.getById(id_field);
     if (!existingField) {
       throw new Error(
         HTTP_HANDLING_MSGS.errorNotFound(
@@ -30,6 +57,7 @@ class FieldService {
         )
       );
     }
+    // Validate that the field is not associated with any campuses
     const fieldInCampus = await FieldRepository.isFieldInCampus(id_field);
     if (fieldInCampus) {
       throw new Error(
