e9cbf575df7e55fd023a2ea53529673e7f443c7b
unknown <nicolaspicon98@gmail.com>
2023-04-09 18:38:01 -0500

    s

--- src/api/v1/production/bienestar/middlewares/validation-rules/auth/auth.middleware.js
+++ src/api/v1/production/bienestar/middlewares/validation-rules/auth/auth.middleware.js
@@ -7,6 +7,9 @@ const loginValidationRules = () => {
       .withMessage("El campo email_user es obligatorio")
       .isEmail()
       .withMessage("El campo email_user debe ser un correo electrónico válido"),
+    body("password_user")
+      .notEmpty()
+      .withMessage("El campo password_user es obligatorio")
   ];
 };
 
