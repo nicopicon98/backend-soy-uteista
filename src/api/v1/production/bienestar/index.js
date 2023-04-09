c814349ad01cabadbeffd494fec66de28bb16457
unknown <nicolaspicon98@gmail.com>
2023-04-08 23:03:28 -0500

    s

--- src/api/v1/production/bienestar/index.js
+++ src/api/v1/production/bienestar/index.js
@@ -5,10 +5,12 @@ const bienestar = express();
 const timeSlotRouter = require("./routes/time-slots");
 const userTimeSlotsDateRouter = require('./routes/user-time-slots-date')
 const userRouter = require('./routes/user')
+const fieldRouter = require('./routes/fields')
 
 bienestar.use(timeSlotRouter);
 bienestar.use(userTimeSlotsDateRouter);
 bienestar.use(userRouter);
+bienestar.use(fieldRouter);
 
 const bienestarVersion = () => "/api/v1/production/bienestar";
 module.exports = {
