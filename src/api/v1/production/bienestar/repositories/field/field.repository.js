4e220bf68c4b371d8037a9152338cf9477827bfe
unknown <nicolaspicon98@gmail.com>
2023-04-08 23:08:21 -0500

    s

--- src/api/v1/production/bienestar/repositories/field/field.repository.js
+++ src/api/v1/production/bienestar/repositories/field/field.repository.js
@@ -8,12 +8,14 @@ class FieldRepository {
 
   static async getAllNotInCampus(id_campus) {
     const query = `
-      SELECT * FROM fields
-      WHERE id_field NOT IN (
-        SELECT id_field FROM campuses_fields
-        WHERE id_campus = ?
+    SELECT f.*
+      FROM fields f
+      WHERE f.id_field NOT IN (
+        SELECT cf.id_field
+        FROM campuses_fields cf
+        WHERE cf.id_campus = ?
       )
-    `;
+  `;
     return mysql.executeQuery(query, [id_campus]);
   }
 
