b646fa81b6636273d717c9a2a5c0ee9ac13ec0cc
unknown <nicolaspicon98@gmail.com>
2023-04-09 12:12:41 -0500

    s

--- src/api/v1/production/bienestar/routes/cryptography/cryptography.routes.js
+++ src/api/v1/production/bienestar/routes/cryptography/cryptography.routes.js
@@ -7,9 +7,13 @@ cryptRouter.post("/deco", (req, res) => {
   sendService(content, res);
 });
 
-cryptRouter.post("/enco", (req, res) => {
-  const content = req.body;
-  sendService(encrypt(JSON.stringify(content)).content, res);
-});
+/**
+ * As info is being encrypted from the very entry point, it must be handled somewhere else
+ * This is being handled on https://webservice.uts.edu.co/api/v1/production/soyuteista/enco
+ */
+// cryptRouter.post("/enco", (req, res) => {
+//   const content = req.body;
+//   sendService(encrypt(JSON.stringify(content)).content, res);
+// });
 
 module.exports = cryptRouter;
