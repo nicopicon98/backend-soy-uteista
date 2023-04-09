9f3dc26f048f1ab7237486d8f78d0c38e4455611
unknown <nicolaspicon98@gmail.com>
2023-04-09 12:09:07 -0500

    s

--- src/api/v1/production/bienestar/routes/cryptography/cryptography.routes.js
+++ src/api/v1/production/bienestar/routes/cryptography/cryptography.routes.js
@@ -1,10 +1,15 @@
-const { sendService } = require("@api_bienestar/config");
+const { sendService, encrypt } = require("@api_bienestar/config");
 const express = require("express");
 const cryptRouter = express.Router();
 
 cryptRouter.post("/deco", (req, res) => {
   const content = req.body;
   sendService(content, res);
+});
+
+cryptRouter.post("/enco", (req, res) => {
+  const content = req.body;
+  sendService(encrypt(JSON.stringify(content)).content, res);
 });
 
 module.exports = cryptRouter;
