966c799f764953967fb4779a62fc7028369bcd3f
unknown <nicolaspicon98@gmail.com>
2023-04-09 18:55:02 -0500

    s

--- src/api/v1/production/bienestar/services/auth/auth.service.js
+++ src/api/v1/production/bienestar/services/auth/auth.service.js
@@ -1,18 +1,23 @@
 const { HTTP_HANDLING_MSGS } = require("../../utilities");
 const AuthRepository = require("../../repositories/auth");
+const {
+  comparePassword,
+} = require("@src/common/security/bcrypt_encryption.js");
 
 class AuthService {
   static async login(email_user, password_user) {
     //evaluate if user exists
     const resp = await AuthRepository.itExists(email_user);
-    if (!resp) {
+    console.log(resp);
+    if (!(resp.length > 0)) {
       throw new Error(
         HTTP_HANDLING_MSGS.errorNotFound(
           `${email_user} no se encuentra registrado`
         )
       );
     }
-    return { message: "it's working" };
+    //evaluate passwords match
+    // const validPassword = comparePassword(password_user);
   }
 }
 
