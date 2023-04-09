31d985de318573c978d5518b4a8c47f587fb735e
unknown <nicolaspicon98@gmail.com>
2023-04-09 18:44:30 -0500

    s

--- src/api/v1/production/bienestar/repositories/auth/auth.repository.js
+++ src/api/v1/production/bienestar/repositories/auth/auth.repository.js
@@ -2,8 +2,10 @@ const { mysql } = require("@src/common/conexiones/conexionMysql");
 
 class AuthRepository {
   static async itExists(email_user) {
+    console.log(email_user, "email_user")
     const query = "SELECT * FROM users WHERE email_user = ?";
     const result = await mysql.executeQuery(query, [email_user]);
+    console.log(result, "result")
     return result.length > 0;
   }
 }
