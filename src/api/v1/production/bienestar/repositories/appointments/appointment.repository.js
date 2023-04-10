aa7fc4b36768407e4941a794aef3deac0b411625
unknown <nicolaspicon98@gmail.com>
2023-04-09 23:49:39 -0500

    s

--- src/api/v1/production/bienestar/repositories/appointments/appointment.repository.js
+++ src/api/v1/production/bienestar/repositories/appointments/appointment.repository.js
@@ -34,28 +34,28 @@ class AppointmentRepository {
 
   static async getLastAppointmentByProfessional(id_user) {
     const query = `
-      SELECT
-        a.photo_student,
-        a.name_student,
-        a.registration_date,
-        a.phone_student,
-        a.attended,
-        ut.date,
-        ts.name_time_slot
-      FROM
-        appointments a
-      JOIN
-        users_time_slots_dates ut
-        ON a.id_user_time_slot_date = ut.id_user_time_slot_date
-      JOIN
-        time_slots ts
-        ON ut.id_time_slot = ts.id_time_slot
-      WHERE
-        ut.date < CURDATE() AND ut.id_user = ? AND a.rejected_by != 1
-      ORDER BY
-        ut.date DESC,
-        SUBSTRING_INDEX(ts.name_time_slot, ' - ', 1) DESC
-      LIMIT 1
+    SELECT
+    a.photo_student,
+    a.name_student,
+    a.registration_date,
+    a.phone_student,
+    a.attended,
+    ut.date,
+    ts.name_time_slot
+  FROM
+    appointments a
+  JOIN
+    users_time_slots_dates ut
+    ON a.id_user_time_slot_date = ut.id_user_time_slot_date
+  JOIN
+    time_slots ts
+    ON ut.id_time_slot = ts.id_time_slot
+  WHERE
+    ut.date < CURDATE() AND ut.id_user = ? AND a.rejected_by IS NULL
+  ORDER BY
+    ut.date DESC,
+    SUBSTRING_INDEX(ts.name_time_slot, ' - ', 1) DESC
+  LIMIT 1;
     `;
     const result = await mysql.executeQuery(query, [id_user]);
     if (!result.length) {
