2d441bf718f535f10ed3b96f8c3824b437d2c62e
unknown <nicolaspicon98@gmail.com>
2023-04-10 17:30:12 -0500

    s

--- src/api/v1/production/bienestar/services/campuses_fields/campus-field.service.js
+++ src/api/v1/production/bienestar/services/campuses_fields/campus-field.service.js
@@ -3,7 +3,26 @@ const UserRepository = require("../../repositories/users");
 
 class CampusFieldService {
   static async getAllByCampus() {
-    return CampusFieldRepository.getAllByCampus();
+    const rawData = await CampusFieldRepository.getAllByCampus();
+    const groupedData = {};
+
+    rawData.forEach((row) => {
+      if (!groupedData[row.id_campus]) {
+        groupedData[row.id_campus] = {
+          id_campus_field: row.id_campus_field,
+          name_campus: row.name_campus,
+          fields: [],
+        };
+      }
+
+      groupedData[row.id_campus].fields.push({
+        id_field: row.id_field,
+        name_field: row.name_field,
+        id_campus_field: row.id_campus_field,
+      });
+    });
+
+    return Object.values(groupedData);
   }
 
   static async insert(id_campus, id_field) {
