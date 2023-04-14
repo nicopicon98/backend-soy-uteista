81016c65b7cf67f10f403218104223ebc59179ca
unknown <nicolaspicon98@gmail.com>
2023-04-14 09:03:03 -0500

    s

--- src/api/v1/production/bienestar/repositories/appointments/appointment.repository.js
+++ src/api/v1/production/bienestar/repositories/appointments/appointment.repository.js
@@ -34,30 +34,36 @@ class AppointmentRepository {
 
   static async getAllUpcomingByProfessional(id_user) {
     const query = `
-      SELECT
-        a.photo_student,
-        a.name_student,
-        a.id_appointment,
-        a.booked_by,
-        a.rejected,
-        a.registration_date,
-        a.phone_student,
-        a.attended,
-        ut.date,
-        ts.name_time_slot
-      FROM
-        appointments a
-      JOIN
-        users_time_slots_dates ut
-        ON a.id_user_time_slot_date = ut.id_user_time_slot_date
-      JOIN
-        time_slots ts
-        ON ut.id_time_slot = ts.id_time_slot
-      WHERE
-        ut.date >= CURDATE() AND ut.id_user = ?
-      ORDER BY
-        ut.date ASC,
-        SUBSTRING_INDEX(ts.name_time_slot, ' - ', 1) ASC`;
+    SELECT
+    a.photo_student,
+    a.name_student,
+    a.id_appointment,
+    a.booked_by,
+    a.rejected,
+    a.rejected_reason,
+    u.name_user AS rejected_by_name,
+    a.registration_date,
+    a.phone_student,
+    a.attended,
+    ut.date,
+    ts.name_time_slot
+FROM
+    appointments a
+JOIN
+    users_time_slots_dates ut
+    ON a.id_user_time_slot_date = ut.id_user_time_slot_date
+JOIN
+    time_slots ts
+    ON ut.id_time_slot = ts.id_time_slot
+LEFT JOIN
+    users u
+    ON a.rejected_by = u.id_user
+WHERE
+    ut.date >= CURDATE() AND ut.id_user = ?
+ORDER BY
+    ut.date ASC,
+    SUBSTRING_INDEX(ts.name_time_slot, ' - ', 1) ASC
+`;
     const result = await mysql.executeQuery(query, [id_user]);
     if (!result.length) {
       return "No tienes citas pendientes";
